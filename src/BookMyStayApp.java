07f65da6ca6fbeca73d2b5c94204a2bdfc35cf53
adhithxya <am0180@srmist.edu.in>
2026-03-18 02:15:55 +0530

    Update BookMyStayApp.java

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,2 +1,84 @@
+/**
+ * @version 2.0
+ */
+
+
+abstract class Room {
+    protected int beds;
+    protected int size;
+    protected double price;
+
+    public Room(int beds, int size, double price) {
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
+    }
+
+    public abstract String getRoomType();
+
+    public void displayDetails(int availability) {
+        System.out.println(getRoomType() + ":");
+        System.out.println("Beds: " + beds);
+        System.out.println("Size: " + size + " sqft");
+        System.out.println("Price per night: " + price);
+        System.out.println("Available: " + availability);
+        System.out.println();
+    }
+}
+
+// Single Room
+class SingleRoom extends Room {
+    public SingleRoom() {
+        super(1, 250, 1500.0);
+    }
+
+    public String getRoomType() {
+        return "Single Room";
+    }
+}
+
+// Double Room
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super(2, 400, 2500.0);
+    }
+
+    public String getRoomType() {
+        return "Double Room";
+    }
+}
+
+// Suite Room
+class SuiteRoom extends Room {
+    public SuiteRoom() {
+        super(3, 750, 5000.0);
+    }
+
+    public String getRoomType() {
+        return "Suite Room";
+    }
+}
+
 public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hotel Room Initialization");
+        System.out.println();
+
+        
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+       
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+       
+        single.displayDetails(singleAvailable);
+        doubleRoom.displayDetails(doubleAvailable);
+        suite.displayDetails(suiteAvailable);
+    }
 }
