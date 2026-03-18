dbcd4264690416ece5109037fa8ecbf6158051fd
adhithxya <am0180@srmist.edu.in>
2026-03-18 08:22:19 +0530

    Rename UseCase9ErrorHandlingValidation to BookMyStayApp

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -75,7 +75,7 @@ class BookingRequestQueue {
 }
 
 // -------------------- MAIN CLASS --------------------
-public class UseCase9ErrorHandlingValidation {
+public class BookMyStayApp {
 
     public static void main(String[] args) {
 
